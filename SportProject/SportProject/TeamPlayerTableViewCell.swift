32ec195adfe0c2299c6f926403495299d233313b
KevinSia <calvin.kl1026@gmail.com>
2016-12-23 11:25:36 +0800

    done naming and linking partially

---
+++ SportProject/SportProject/TeamPlayerTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  TeamPlayerTableViewCell.swift
+//  SportProject
+//
+//  Created by Students on 12/23/16.
+//  Copyright © 2016 Calvin.kl. All rights reserved.
+//
+
+import UIKit
+
+class TeamPlayerTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
